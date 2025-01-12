40f71cc61097ab2915ba8f3b3a64ae896de96708
Gareth Erskine-Jones <gsej@erskinejones.com>
2025-01-12 13:15:42 +0000

    remove popup

--- projects/gsej/tailwind-components/src/public-api.ts
+++ projects/gsej/tailwind-components/src/public-api.ts
@@ -9,5 +9,4 @@ export * from './lib/card-header/card-header.component';
 export * from './lib/card-title/card-title.component';
 export * from './lib/form/form-label.component';
 export * from './lib/header/header.component';
-export * from './lib/popup/popup.component';
 export * from './lib/separator/separator.component';
